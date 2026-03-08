f97c774308bc95d5e36050852343dea145ab5231
Rolex-hub <goswamianuj881@gmail.com>
2026-03-08 12:50:27 +0530

    Fix MySQL connection for Render deployment

--- database.py
+++ database.py
@@ -4,11 +4,10 @@ import os
 mysql = MySQL()
 
 def init_db(app):
-
-    app.config['MYSQL_HOST'] = os.getenv("MYSQLHOST")
-    app.config['MYSQL_USER'] = os.getenv("MYSQLUSER")
-    app.config['MYSQL_PASSWORD'] = os.getenv("MYSQLPASSWORD")
-    app.config['MYSQL_DB'] = os.getenv("MYSQLDATABASE")
+    app.config['MYSQL_HOST'] = os.getenv("MYSQLHOST") or os.getenv("MYSQL_PUBLIC_URL")
+    app.config['MYSQL_USER'] = os.getenv("MYSQLUSER") or "root"
+    app.config['MYSQL_PASSWORD'] = os.getenv("MYSQLPASSWORD") or os.getenv("MYSQL_ROOT_PASSWORD")
+    app.config['MYSQL_DB'] = os.getenv("MYSQLDATABASE") or os.getenv("MYSQL_DATABASE")
     app.config['MYSQL_PORT'] = int(os.getenv("MYSQLPORT", "3306"))
 
     mysql.init_app(app)
