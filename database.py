79570bdf08ecd451e6122b7f57cdeef58a5b210c
Rolex-hub <goswamianuj881@gmail.com>
2026-03-08 14:36:14 +0530

    Add database.py

--- database.py
+++ database.py
@@ -1,13 +1,11 @@
 from flask_mysqldb import MySQL
-import os
 
 mysql = MySQL()
 
 def init_db(app):
-    app.config['MYSQL_HOST'] = os.getenv("MYSQLHOST") or os.getenv("MYSQL_PUBLIC_URL")
-    app.config['MYSQL_USER'] = os.getenv("MYSQLUSER") or "root"
-    app.config['MYSQL_PASSWORD'] = os.getenv("MYSQLPASSWORD") or os.getenv("MYSQL_ROOT_PASSWORD")
-    app.config['MYSQL_DB'] = os.getenv("MYSQLDATABASE") or os.getenv("MYSQL_DATABASE")
-    app.config['MYSQL_PORT'] = int(os.getenv("MYSQLPORT", "3306"))
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'flaskuser'
+    app.config['MYSQL_PASSWORD'] = 'Kalki'
+    app.config['MYSQL_DB'] = 'grainwala_db'
 
     mysql.init_app(app)
